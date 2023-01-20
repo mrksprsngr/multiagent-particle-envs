90abcb8ccbbc32c5d62778897f3ed9f3e882a784
Sven Gronauer <sven.gronauer@tum.de>
2023-01-20 15:47:18 +0100

    Updated: multi-agent play script

--- bin/play_random.py
+++ bin/play_random.py
@@ -1,20 +1,24 @@
 #!/usr/bin/env python
 import gym
 import argparse
-import multiagent
+import multiagent  # noqa
 
 if __name__ == '__main__':
     # parse arguments
-    parser = argparse.ArgumentParser(description=None)
-    parser.add_argument('-e', '--env', default='"Simplespread-v2"',
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--env', default='SimpleSpread-v0',
                         help='Name of the environment that should be played.')
     args = parser.parse_args()
-
+    print(args.env)
     env = gym.make(args.env)
 
-    x = env.reset()
-    for i in range(100):
-        env.render()
-        action = env.action_space.sample()
-        env.step(action)
+    while True:
+        x = env.reset()
+        for i in range(100):
+            env.render()
+            actions = []
+            for acs in env.action_space:
+                action = acs.sample()
+                actions.append(action)
+            env.step(actions)
 
