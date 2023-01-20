2d73aeb3bd257dda51fbe7cd948d97c872c95f2f
Sven Gronauer <sven.gronauer@tum.de>
2023-01-20 15:47:05 +0100

    Renamed: to SimpleSpread-v0

--- multiagent/__init__.py
+++ multiagent/__init__.py
@@ -1,25 +1,7 @@
-import os
-import warnings
-
 from gym.envs.registration import register
 
 # Multiagent envs
 # ----------------------------------------
-
-register(
-    id='MultiagentSimple-v0',
-    entry_point='multiagent.envs:SimpleEnv',
-    # FIXME(cathywu) currently has to be exactly max_path_length parameters in
-    # rllab run script
-    max_episode_steps=100,
-)
-
-register(
-    id='MultiagentSimpleSpeakerListener-v0',
-    entry_point='multiagent.envs:SimpleSpeakerListenerEnv',
-    max_episode_steps=100,
-)
-
 
 register(
     id='Simple-v0',
@@ -28,7 +10,7 @@ register(
 )
 
 register(
-    id='Simplespread-v2',
+    id='SimpleSpread-v0',
     entry_point='multiagent.envs.simple_spread:SimpleSpreadEnv',
-    max_episode_steps=100,
+    max_episode_steps=500,
 )
