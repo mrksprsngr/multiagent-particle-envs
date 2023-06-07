dcb54cd35fbb1d3b52a515ab193c01a26fe21e2f
Sven Gronauer <sven.gronauer@tum.de>
2023-06-07 14:45:28 +0200

    Updated: play simple env script

--- bin/play_simple_env.py
+++ bin/play_simple_env.py
@@ -5,12 +5,16 @@ import multiagent  # noqa
 if __name__ == '__main__':
     env = gym.make("Simple-v0")
 
-    x = env.reset()
-    for i in range(3):
-        #env.render()
-        action = env.action_space.sample()
-        print(action)
-        env.step(action)
-        print(action)
+    for i in range(10):
+        # start a new eposide
+        x = env.reset()
+
+        N = 100
+        for _ in range(100):
+            # run N steps for each episode
+            env.render()
+            action = env.action_space.sample()
+            print(action)
+            env.step(action)
     env.close()
 
